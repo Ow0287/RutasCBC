a61db1dc966040384b0286a164703404479355b4
deivisosorio3 <deiviseguiso@gmail.com>
2017-08-13 16:42:15 -0500

    ya oscar

---
+++ app/src/main/java/com/misena/oscar/rutascbc/RegistroLogin.java
@@ -0,0 +1,13 @@
+package com.misena.oscar.rutascbc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistroLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_login);
+    }
+}
