0fecdce708266ad1c74b74c944c6ffe78e78b2dd
deivisosorio3 <deiviseguiso@gmail.com>
2017-08-15 17:30:23 -0500

    qwert

---
+++ app/src/main/java/com/misena/oscar/rutascbc/GaleriaAministrador.java
@@ -0,0 +1,13 @@
+package com.misena.oscar.rutascbc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GaleriaAministrador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_galeria_aministrador);
+    }
+}
